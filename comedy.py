43db67ff3fffd3a41bff2a5977abc3d5f6a2d0ba
Aaron Culich <aculich@eecs.berkeley.edu>
2013-01-27 22:14:14 -0800

    joining a list of strings with join()

--- comedy.py
+++ comedy.py
@@ -4,7 +4,8 @@ datadir = 'Data'
 
 def main():
     subdir = 'comedy_comparisons'
-    print datadir + '/' + subdir
+    test_file = 'comedy_comparisons.test'
+    print '/'.join([datadir, subdir, test_file])
 
 if __name__ == "__main__":
     main()
