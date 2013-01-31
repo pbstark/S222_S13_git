153eecdb0a85a416c4c73ef9ad0af637f5b37cf5
Aaron Culich <aculich@eecs.berkeley.edu>
2013-01-31 06:58:54 -0800

    opening and closing files using the 'with' statement

--- comedy.py
+++ comedy.py
@@ -8,9 +8,8 @@ def main():
     subdir = 'comedy_comparisons'
     test_file = 'comedy_comparisons.test'
     test_data = os.path.join(datadir, subdir, test_file)
-    csvfile = open(test_data, 'r')
-    print csvfile
-    csvfile.close()
+    with open(test_data, 'r') as csvfile:
+        print csvfile
     print csvfile
 
 if __name__ == "__main__":
