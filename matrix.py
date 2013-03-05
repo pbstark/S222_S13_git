c3301a2f780ea22aa0c01b7b5c05aedf410f6d28
Weinan Wang <wangweinan1115@gmail.com>
2013-03-05 09:27:54 -0800

    cleaned the code in matrix.py

--- matrix.py
+++ matrix.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import csv
 from itertools import islice
 from logging import debug, info ,error
@@ -8,6 +9,7 @@ import gdata.youtube
 import gdata.youtube.service
 import numpy as np
 import json, ast
+
 
 from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import spsolve
@@ -22,6 +24,9 @@ def normalize(a, b, key):
 		return [a,b]
 	else:
 		return [b,a]
+
+def clean(s,x=3):
+	return str(s)[x:-x]
 
 def comedy_unique_id(conn):
 	subdir = 'comedy_comparisons'
@@ -51,36 +56,24 @@ def comedy_unique_id(conn):
 		UNION ALL
 		SELECT id2 AS id  FROM com
 		)AS temp;""")
-		uniqid = []
 		uniq =  c.fetchall()
-		for line in uniq:
-			line = str(line)
-			line = line[3:-3]
-			uniqid.append(line)
-		#for line in uniq:
-		#	line = line[2:]
-		#	print line
-		label = range(1,18475)
-		dic = dict(zip(uniqid,label))
+
+		dic = dict([(clean(b),a) for (a,b) in enumerate(uniq)])
 		c.execute("""SELECT id1, id2, COUNT(*)
 		FROM com GROUP BY id1, id2
 		ORDER BY COUNT(*) DESC;""")
+		#uniqpair_len = c.rowcount
 		uniqpair = c.fetchall()
 		#print uniqpair[1][1]
 
-		A = lil_matrix((18474,18474))
+		A = lil_matrix((len(dic),len(dic)))
 		# Upper triangle means rows funnier than columns; Vice versa
-		for row in range(1,358979):
-				A[dic[uniqpair[row-1][0]] - 1,dic[uniqpair[row-1][1]] - 1] = uniqpair[row-1][2]
+		for row in uniqpair:
+				A[dic[row[0]],dic[row[1]]] = row[2]
 		print A[0:2000,0:2000]
 
 
 		
-
-
-		
-
-
 def main():
 	with sqlite3.connect(':memory:') as conn: #With is gonna guarantee it's gonna close automatically
 		 comedy_unique_id(conn)
