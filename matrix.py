d2b8ea41132462f5b3f3c2e6e7d74c79a02ed766
Weinan Wang <wangweinan1115@gmail.com>
2013-03-05 09:42:55 -0800

    plot adjacency graph

--- matrix.py
+++ matrix.py
@@ -72,7 +72,45 @@ def comedy_unique_id(conn):
 				A[dic[row[0]],dic[row[1]]] = row[2]
 		print A[0:2000,0:2000]
 
+import networkx as nx
+from matplotlib import pyplot, patches
 
+def draw_adjacency_matrix(G, node_order=None, partitions=[], colors=[]):
+    """
+    - G is a netorkx graph
+    - node_order (optional) is a list of nodes, where each node in G
+          appears exactly once
+    - partitions is a list of node lists, where each node in G appears
+          in exactly one node list
+    - colors is a list of strings indicating what color each
+          partition should be
+    If partitions is specified, the same number of colors needs to be
+    specified.
+    """
+    adjacency_matrix = nx.to_numpy_matrix(G, dtype=np.bool, nodelist=node_order)
+
+    #Plot adjacency matrix in toned-down black and white
+    fig = pyplot.figure(figsize=(5, 5)) # in inches
+    pyplot.imshow(adjacency_matrix,
+                  cmap="Greys",
+                  interpolation="none")
+    
+    # The rest is just if you have sorted nodes by a partition and want to
+    # highlight the module boundaries
+    assert len(partitions) == len(colors)
+    ax = pyplot.gca()
+    for partition, color in zip(partitions, colors):
+        current_idx = 0
+        for module in partition:
+            ax.add_patch(patches.Rectangle((current_idx, current_idx),
+                                          len(module), # Width
+                                          len(module), # Height
+                                          facecolor="none",
+                                          edgecolor=color,
+                                          linewidth="1"))
+            current_idx += len(module)
+
+draw_adjacency_matrix(nx.from_scipy_sparse_matrix(A))
 		
 def main():
 	with sqlite3.connect(':memory:') as conn: #With is gonna guarantee it's gonna close automatically
